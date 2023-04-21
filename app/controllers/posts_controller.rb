2dd5a9aca2a0d19c40e50b1cf567a091adf88c2a
Koi115 <chiiuko115@gmail.com>
2023-04-21 22:36:12 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,9 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    binding.pry
+    post = Post.create(content: params[:content])
+    # redirect_to action: :index  # 追記する
+    render json:{ post: post }    
   end
 end
